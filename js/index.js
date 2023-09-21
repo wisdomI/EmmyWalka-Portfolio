4e4fcdc2b3446d8436945afce99090aaf1f8c577
wisdomi <wisdomiwunwa@gmail.com>
2023-09-21 02:23:11 +0100

    adjustments and fix

--- js/index.js
+++ js/index.js
@@ -1,30 +1,13 @@
-// NavBar functionality
-const primaryNav = document.querySelector(".primary-navigation")
-const navToggle = document.querySelector(".mobile-nav-toggle")
-
-navToggle.addEventListener("click", () => {
-  const visibility = primaryNav.getAttribute("data-visible")
-
-  if (visibility === "false") {
-    primaryNav.setAttribute("data-visible", true)
-    navToggle.setAttribute("aria-expanded", true)
-  } else if (visibility === "true") {
-    primaryNav.setAttribute("data-visible", false)
-    navToggle.setAttribute("aria-expanded", false)
-  }
-})
-// ______________________________________________________________________
-
 // Buy Art button functionality
 const buyArt = document.querySelector(".buy-art-btn")
 const closeArt = document.querySelector(".pop-up-close-btn")
 const popUp = document.querySelector(".pop-up-wrapper")
 
 buyArt.addEventListener("click", () => {
-    popUp.classList.add("show")
+  popUp.classList.add("show")
 })
 
 closeArt.addEventListener("click", () => {
-    popUp.classList.remove("show")
+  popUp.classList.remove("show")
 })
 //_________________________________________________________________________
---
+++ js/nav.js
@@ -0,0 +1,25 @@
+// NavBar functionality
+const primaryNav = document.querySelector(".primary-navigation")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible")
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true)
+        navToggle.setAttribute("aria-expanded", true)
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false)
+        navToggle.setAttribute("aria-expanded", false)
+    }
+})
+
+// Nav Link Active
+const activePage = window.location.pathname
+const navLinks = document.querySelectorAll("nav a")
+
+navLinks.forEach(element => {
+    if (element.href.includes(`${activePage}`)) {
+        element.classList.add("active")
+    }
+});
